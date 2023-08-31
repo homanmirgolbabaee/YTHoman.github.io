7188742b130fde0d3fe23bb1319d6fbaac5c88ac
Homan Mirgolbabaee <homanmirgolbabaee@gmail.com>
2023-08-31 16:48:49 +0200

    Update youtube-downloader-main-Core.py

--- youtube-downloader-main-Core.py
+++ youtube-downloader-main-Core.py
@@ -1,59 +1,77 @@
 import streamlit as st
 from pytube import YouTube
-import time
+import os
+import re
 
-# Set Streamlit page configuration
-st.set_page_config(page_title="Mokhlese Mehdi's Youtube Video Downloader", page_icon="🎥")
+directory= 'downloads/'
+if not os.path.exists(directory):
+    os.makedirs(directory)
+    
+st.set_page_config(page_title="YTD ", page_icon="🚀", layout="wide", )     
+st.markdown(f"""
+            <style>
+            .stApp {{ 
+                     background-attachment: fixed;
+                     background-size: cover}}
+         </style>
+         """, unsafe_allow_html=True)
 
-def main():
-    st.title("Mokhlese Mehdi's Youtube Video Downloader")
-    st.markdown("Download YouTube videos easily! 🎬 by HOMAN 🧑‍💻")
-    st.write("")
+@st.cache(allow_output_mutation=True)
+def get_info(url):
+    yt = YouTube(url)
+    streams= yt.streams.filter(progressive= True, type= 'video')
+    details= {}
+    details["image"]= yt.thumbnail_url
+    details["streams"]= streams
+    details["title"]= yt.title
+    details["length"]= yt.length
+    itag, resolutions, vformat, frate = ([] for i in range(4))
+    for i in streams:
+        res= re.search(r'(\d+)p', str(i))
+        typ= re.search(r'video/(\w+)', str(i))
+        fps= re.search(r'(\d+)fps', str(i))
+        tag= re.search(r'(\d+)',str(i))
+        itag.append(str(i)[tag.start():tag.end()])
+        resolutions.append(str(i)[res.start():res.end()])
+        vformat.append(str(i)[typ.start():typ.end()])
+        frate.append(str(i)[fps.start():fps.end()])
+    details["resolutions"]= resolutions
+    details["itag"]= itag
+    details["fps"]= frate
+    details["format"]= vformat
+    return details
 
-    # Get the video URL from user input
-    video_url = st.text_input("Enter the YouTube video URL:", help="Paste the URL here")
-
-    # Check if the URL is valid before proceeding
-    if st.button("Download"):
-        if not video_url:
-            st.warning("Please enter a YouTube video URL. 🙄")
-            return
-
-        try:
-            yt = YouTube(video_url)
-            st.subheader("Video Information")
-            st.write(f"**Title:** {yt.title}")
-            st.write(f"**Duration:** {yt.length} seconds")
-
-            # Choose the highest resolution stream
-            stream = yt.streams.get_highest_resolution()
-
-            # Display video thumbnail with improved styling
-            thumbnail_html = f"""
-            <div style="
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                max-width: 200px;
-                border: 1px solid #ccc;
-                border-radius: 10px;
-                padding: 10px;
-                box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.2);
-            ">
-                <img src="{yt.thumbnail_url}" alt="Video Thumbnail" style="width: 100%; border-radius: 10px;">
-                <p style="font-weight: bold; margin-top: 10px;">{yt.title} 📽️</p>
-            </div>
-            """
-            st.markdown(thumbnail_html, unsafe_allow_html=True)
-
-            # Download the video
-            with st.spinner("Downloading... ⏳"):
-                stream.download(output_path='downloads')  # Change the output path if needed
-                time.sleep(2)  # Simulate a delay
-            st.success("Download complete! ✅")
-        except Exception as e:
-            st.error(f"An error occurred: {e} ❌")
-
-if __name__ == "__main__":
-    main()
+st.title("خر آقا مهدی ( نسخه یوتوب) 🚀")
+url = st.text_input("Paste URL here 👇", placeholder='https://www.youtube.com/')
+if url:
+    v_info= get_info(url)
+    col1, col2= st.columns([1,1.5], gap="small")
+    with st.container():
+        with col1:            
+            st.image(v_info["image"])   
+        with col2:
+            st.subheader("Video Details ⚙️")
+            res_inp = st.selectbox('__Select Resolution__', v_info["resolutions"])
+            id = v_info["resolutions"].index(res_inp)            
+            st.write(f"__Title:__ {v_info['title']}")
+            st.write(f"__Length:__ {v_info['length']} sec")
+            st.write(f"__Resolution:__ {v_info['resolutions'][id]}")
+            st.write(f"__Frame Rate:__ {v_info['fps'][id]}")
+            st.write(f"__Format:__ {v_info['format'][id]}")
+            file_name = st.text_input('__Save as 🎯__', placeholder = v_info['title'])
+            if file_name:        
+                if file_name != v_info['title']:
+                    file_name+=".mp4"
+            else:
+                file_name = v_info['title'] + ".mp4" 
+                
+        button = st.button("Download ⚡️")
+        if button:
+            with st.spinner('Downloading...'):
+                try:
+                    ds = v_info["streams"].get_by_itag(v_info['itag'][id])
+                    ds.download(filename= file_name, output_path= "downloads/")
+                    st.success('Download Complete', icon="✅")       
+                    st.balloons()
+                except:
+                    st.error('Error: Save with a different name!', icon="🚨")   
